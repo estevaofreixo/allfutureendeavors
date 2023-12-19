97a360e28af4aed9e1e4e831359f738ab183943a
estevaofreixo <153137131+estevaofreixo@users.noreply.github.com>
2023-12-19 14:55:17 -0300

    envio de arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+const botoes = document.getElementsByClassName('btn');
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener('click', function() {
+        
+        alert('not ready yet!');
+    });
+}
